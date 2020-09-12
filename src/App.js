f1ddce15887aa6ee3e7a3ef5f91587315cdbefc7
khan4200000 <www.muktarul420@gmail.com>
2020-09-12 15:17:20 +0600

    one error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Book from './components/Book/Book';
+import Header from './components/Header/Header';
+import PrivetRoute from './components/PrivetRoute/PrivetRoute';
+
+
+export  const UserContext = createContext();
 
 function App() {
+ const[loggedInUser,setLoggedInUser] = useState({});
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[loggedInUser,setLoggedInUser]}>
+    <h4>Name:{loggedInUser.name}</h4>
+    <h5>Email:{loggedInUser.email}</h5>
+    
+    <Router>
+    <Header/>
+    <Switch>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <PrivetRoute path="/book/:bedType">
+        <Book />
+      </PrivetRoute>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+</Router>
+</UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGvpAWl4XNgKt8AuVKLyH3q8M6eCaVgOc",
+    authDomain: "luxurious-burj-al-arab.firebaseapp.com",
+    databaseURL: "https://luxurious-burj-al-arab.firebaseio.com",
+    projectId: "luxurious-burj-al-arab",
+    storageBucket: "luxurious-burj-al-arab.appspot.com",
+    messagingSenderId: "213625514017",
+    appId: "1:213625514017:web:6fcfcacb54649fc4249b60"
+  };
+  export default firebaseConfig;
+ 
